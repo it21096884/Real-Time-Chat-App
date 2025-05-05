7f42c224850b4a62f6b92a87801d1b259a4bfe78
it21096884 <it21096884@my.sliit.lk>
2025-05-05 14:22:05 +0530

    profile page size

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());//middleware to parse json data
+app.use(express.json({ limit: '10mb' }));//middleware to parse json data
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser()); // allow to use the cookie parser
 app.use(
     cors({
